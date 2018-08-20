965e1b284a76b47d5feaba10dcaf13e1f90e053d
LiuYemao <lym25517235@gmail.com>
2018-08-20 23:55:25 +0800

    drop the rows which take no expressing gene.

--- preExp.py
+++ preExp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import numpy as np
 import sys
 
 def parseExp(s):
@@ -35,20 +36,20 @@ def main(wfname):
                     bogenes.append(ll[1])
                     bnaA.append(ll[2])
                     bnaC.append(ll[3])
-    br_value = [bra.loc[i, 'g_m'] for i in brgenes]
-    bo_value = [bol.loc[i, 'g_m'] for i in bogenes]
-    TH008_Avalue = [TH008.loc[i, 'g_m'] for i in bnaA]
-    TH008_Cvalue = [TH008.loc[i, 'g_m'] for i in bnaC]
-    TD013_Avalue = [TD013.loc[i, 'g_m'] for i in bnaA]
-    TD013_Cvalue = [TD013.loc[i, 'g_m'] for i in bnaC]
-    TH363_Avalue = [TH363.loc[i, 'g_m'] for i in bnaA]
-    TH363_Cvalue = [TH363.loc[i, 'g_m'] for i in bnaC]
-    TH429_Avalue = [TH429.loc[i, 'g_m'] for i in bnaA]
-    TH429_Cvalue = [TH429.loc[i, 'g_m'] for i in bnaC]
-    TH430_Avalue = [TH430.loc[i, 'g_m'] for i in bnaA]
-    TH430_Cvalue = [TH430.loc[i, 'g_m'] for i in bnaC]
-    TH433_Avalue = [TH433.loc[i, 'g_m'] for i in bnaA]
-    TH433_Cvalue = [TH433.loc[i, 'g_m'] for i in bnaC]
+    br_value = [bra.loc[i, 'g_m'] if i in bra.index else np.nan for i in brgenes]
+    bo_value = [bol.loc[i, 'g_m'] if i in bol.index else np.nan for i in bogenes]
+    TH008_Avalue = [TH008.loc[i, 'g_m'] if i in TH008.index else np.nan for i in bnaA]
+    TH008_Cvalue = [TH008.loc[i, 'g_m'] if i in TH008.index else np.nan for i in bnaC]
+    TD013_Avalue = [TD013.loc[i, 'g_m'] if i in TD013.index else np.nan for i in bnaA]
+    TD013_Cvalue = [TD013.loc[i, 'g_m'] if i in TD013.index else np.nan for i in bnaC]
+    TH363_Avalue = [TH363.loc[i, 'g_m'] if i in TH363.index else np.nan for i in bnaA]
+    TH363_Cvalue = [TH363.loc[i, 'g_m'] if i in TH363.index else np.nan for i in bnaC]
+    TH429_Avalue = [TH429.loc[i, 'g_m'] if i in TH429.index else np.nan for i in bnaA]
+    TH429_Cvalue = [TH429.loc[i, 'g_m'] if i in TH429.index else np.nan for i in bnaC]
+    TH430_Avalue = [TH430.loc[i, 'g_m'] if i in TH430.index else np.nan for i in bnaA]
+    TH430_Cvalue = [TH430.loc[i, 'g_m'] if i in TH430.index else np.nan for i in bnaC]
+    TH433_Avalue = [TH433.loc[i, 'g_m'] if i in TH433.index else np.nan for i in bnaA]
+    TH433_Cvalue = [TH433.loc[i, 'g_m'] if i in TH433.index else np.nan for i in bnaC]
 
     dat = {"genes_Br":brgenes, "FPKM_Br":br_value, 'genes_Bo':bogenes, 'FPKM_Bo':bo_value,
            'genes_BnA':bnaA, 'FPKM_TD013_A':TD013_Avalue, 'FPKM_TH008_A':TH008_Avalue,
@@ -58,7 +59,8 @@ def main(wfname):
            'FPKM_TH363_C': TH363_Cvalue, 'FPKM_TH429_C': TH429_Cvalue, 'FPKM_TH430_C': TH430_Cvalue,
            'FPKM_TH433_C': TH433_Cvalue}
     df = pd.DataFrame(dat)
-    df.to_csv(wfname, index=False)
+    mydf = df.dropna(axis=0, how='any')
+    mydf.to_csv(wfname, index=False)
 
 if __name__ == '__main__':
 
